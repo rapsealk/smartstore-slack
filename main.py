4649fe08d4f6c54eb5c272b18518cfc06d1f324e
Jeongseok Kang <piono623@naver.com>
2023-08-14 10:16:18 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,12 @@ from slack_sdk.errors import SlackApiError
 
 SLACK_BOT_TOKEN = os.environ.get("SLACK_BOT_TOKEN")
 
-TITLE = "젤다의 전설 시간의 오카리나 3D"
-URL = "https://smartstore.naver.com/gameswitch/products/4690559023"
+# TITLE = "젤다의 전설 시간의 오카리나 3D"
+# URL = "https://smartstore.naver.com/gameswitch/products/4690559023"
 # TITLE = "젤다의 전설 무쥬라의 가면 3D"
 # URL = "https://smartstore.naver.com/gameswitch/products/4108361209"
+TITLE = "케이던스 오브 하이랄: 크립트 오브 더 네크로댄서"
+URL = "https://smartstore.naver.com/techline/products/5133701323"
 
 
 def post_slack_message(message: str, *, token: str) -> None:
